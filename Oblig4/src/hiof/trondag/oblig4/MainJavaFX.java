adb4b4cb483af2ed6f54b9920c9155884fa38f83
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2019-02-19 13:29:59 +0100

    implementertJavaFX

--- Oblig4/src/hiof/trondag/oblig4/MainJavaFX.java
+++ Oblig4/src/hiof/trondag/oblig4/MainJavaFX.java
@@ -3,6 +3,9 @@ package hiof.trondag.oblig4;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.Scene;
 
 public class MainJavaFX extends Application {
 
@@ -11,8 +14,22 @@ public class MainJavaFX extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage){
-        Label hei = new Label();
-        hei.setText("Hællæ JavaFX");
+    public void start(Stage primaryStage) throws Exception {
+        Label etTekstFelt = new Label();
+        etTekstFelt.setText("Hællæ JavaFX");
+
+        Button enKnapp = new Button();
+        enKnapp.setText("En fin liten knapp! Klikk meg gjerne!");
+
+        VBox hovedLayout = new VBox();
+        hovedLayout.setFillWidth(true);
+        hovedLayout.getChildren().add(etTekstFelt);
+        hovedLayout.getChildren().add(enKnapp);
+
+        Scene hovedScenen = new Scene(hovedLayout, 1000, 600);
+
+        primaryStage.setScene(hovedScenen);
+        primaryStage.setTitle("Min første JavaFX applikasjon");
+        primaryStage.show();
     }
 }
