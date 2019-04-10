b5367dc5baa85b6b0fd870485b7207115b14cd33
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2019-04-10 14:12:42 +0200

    fraBarbar

---
+++ Oblig5Alt1_javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"hiof.trondag.oblig5_alt1"},{"l":"hiof.trondag.oblig5_alt1.controller"},{"l":"hiof.trondag.oblig5_alt1.data"},{"l":"hiof.trondag.oblig5_alt1.model"}]
