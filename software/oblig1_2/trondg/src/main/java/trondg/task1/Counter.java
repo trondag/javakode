178d7f649d0f7485dd6512fd47b85d6d5a64e2a9
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2019-09-29 15:51:16 +0200

    passed - sendte inn 3, returnerte 3

--- software/oblig1_2/trondg/src/main/java/trondg/task1/Counter.java
+++ software/oblig1_2/trondg/src/main/java/trondg/task1/Counter.java
@@ -3,11 +3,7 @@ package trondg.task1;
 class Counter {
 
     static int count(int number){
-        switch (number){
-            case 1 : return 1;
-            case 2 : return 2;
-        }
-        return 0;
+        return number;
     }
 
 }
