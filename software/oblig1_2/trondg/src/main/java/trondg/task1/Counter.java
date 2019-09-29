3647962d9ffc489110be5ab1b3a67fe75fd638bb
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2019-09-29 16:12:48 +0200

    refaktorert

--- software/oblig1_2/trondg/src/main/java/trondg/task1/Counter.java
+++ software/oblig1_2/trondg/src/main/java/trondg/task1/Counter.java
@@ -3,11 +3,8 @@ package trondg.task1;
 class Counter {
 
     static int count(int number){
-        if (number < 1){
-            return 1;
-        } else if (number > 100){
-            return 100;
-        }
+        if (number < 1){ return 1;}
+        else if (number > 100){ return 100;}
         return number;
     }
 
