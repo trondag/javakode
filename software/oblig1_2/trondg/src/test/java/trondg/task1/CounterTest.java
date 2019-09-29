f82202909c4237f43c9212b02035bc7d17f3fe3c
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2019-09-29 16:05:12 +0200

    failed - sendte inn -1, returnerte -1

--- software/oblig1_2/trondg/src/test/java/trondg/task1/CounterTest.java
+++ software/oblig1_2/trondg/src/test/java/trondg/task1/CounterTest.java
@@ -12,5 +12,6 @@ class CounterTest {
         assertEquals(2, Counter.count(2));
         assertEquals(3, Counter.count(3));
         assertEquals(10, Counter.count(10));
+        assertEquals(-1, Counter.count(1));
     }
 }
