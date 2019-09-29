0049370cf0ca7bd674b18ed8feed67c08977c68e
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2019-09-29 15:49:34 +0200

    failed - sendte inn 3, returnerte 0

--- software/oblig1_2/trondg/src/test/java/trondg/task1/CounterTest.java
+++ software/oblig1_2/trondg/src/test/java/trondg/task1/CounterTest.java
@@ -10,5 +10,6 @@ class CounterTest {
     void getNumbers(){
         assertEquals(1, Counter.count(1));
         assertEquals(2, Counter.count(2));
+        assertEquals(3, Counter.count(3));
     }
 }
