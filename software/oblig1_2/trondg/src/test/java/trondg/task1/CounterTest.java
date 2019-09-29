e085a03243bcca9783e9bb97d89fe8f653cdc5fe
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2019-09-29 16:08:59 +0200

    passed - sendte inn 120, returnerte 100

--- software/oblig1_2/trondg/src/test/java/trondg/task1/CounterTest.java
+++ software/oblig1_2/trondg/src/test/java/trondg/task1/CounterTest.java
@@ -13,5 +13,6 @@ class CounterTest {
         assertEquals(3, Counter.count(3));
         assertEquals(10, Counter.count(10));
         assertEquals(1, Counter.count(-1));
+        assertEquals(100, Counter.count(120));
     }
 }
