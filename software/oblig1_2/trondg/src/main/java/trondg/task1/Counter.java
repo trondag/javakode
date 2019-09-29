c44d736ebec7ff1ce2866e90f38bb8299d050ca4
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2019-09-29 16:07:42 +0200

    passed - sendte inn -1, returnerte 1

--- software/oblig1_2/trondg/src/main/java/trondg/task1/Counter.java
+++ software/oblig1_2/trondg/src/main/java/trondg/task1/Counter.java
@@ -3,6 +3,11 @@ package trondg.task1;
 class Counter {
 
     static int count(int number){
+        if (number < 1){
+            return 1;
+        } else if (number > 100){
+            return 100;
+        }
         return number;
     }
 
