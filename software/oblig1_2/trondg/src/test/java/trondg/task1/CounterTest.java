7f5eb897d82acddc83f592c05e39cfab19b1c4d6
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2019-10-09 16:51:17 +0200

    Test failet, forventet Hi tilbake da jeg sendte inn 3

--- software/oblig1_2/trondg/src/test/java/trondg/task1/CounterTest.java
+++ software/oblig1_2/trondg/src/test/java/trondg/task1/CounterTest.java
@@ -8,11 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CounterTest {
     @Test
     void getNumbers(){
-        assertEquals(1, Counter.count(1));
-        assertEquals(2, Counter.count(2));
-        assertEquals(3, Counter.count(3));
-        assertEquals(10, Counter.count(10));
-        assertEquals(1, Counter.count(-1));
-        assertEquals(100, Counter.count(120));
+        assertEquals("1", Counter.count(1));
+        assertEquals("2", Counter.count(2));
+        assertEquals("10", Counter.count(10));
+        assertEquals("1", Counter.count(-1));
+        assertEquals("100", Counter.count(120));
+
+        assertEquals("Hi", Counter.count(3));
     }
 }
